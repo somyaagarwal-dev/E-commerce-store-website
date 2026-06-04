049009c75bfeff652ad6dcb42993aa496a6f82e8
Somya Agarwal <somyaagarwal.dev@gmail.com>
2026-06-04 15:24:07 +0530

    Add search and voice search features

--- script.js
+++ script.js
@@ -21,3 +21,81 @@ function addToCart(productName, price) {
 }
 console.log(productName + " added to cart!");
 }
+function searchProducts() {
+
+    let input =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let products =
+        document.querySelectorAll(".products-card");
+
+    let found = false;
+
+    products.forEach(product => {
+
+        let name =
+            product.querySelector("h3")
+            .innerText
+            .toLowerCase();
+
+        let description =
+            product.querySelector("p")
+            .innerText
+            .toLowerCase();
+
+        if(
+            name.includes(input)
+            ||
+            description.includes(input)
+        ){
+            product.style.display = "block";
+            found = true;
+        }
+        else{
+            product.style.display = "none";
+        }
+
+    });
+
+    document.getElementById("no-results")
+        .style.display =
+            found ? "none" : "block";
+}
+function startVoiceSearch() {
+
+    const SpeechRecognition =
+        window.SpeechRecognition ||
+        window.webkitSpeechRecognition;
+
+    if (!SpeechRecognition) {
+        alert("Voice search is not supported in this browser.");
+        return;
+    }
+
+    const recognition = new SpeechRecognition();
+
+    recognition.lang = "en-US";
+    recognition.interimResults = false;
+
+    recognition.start();
+
+    recognition.onresult = function(event) {
+
+    let speech =
+    event.results[0][0].transcript
+    .replace(/[.,!?]/g, "")
+    .trim();
+
+        document.getElementById("searchInput").value =
+            speech;
+
+        searchProducts();
+    };
+
+    recognition.onerror = function(event) {
+        console.log(event.error);
+        alert("Voice search error: " + event.error);
+    };
+}
